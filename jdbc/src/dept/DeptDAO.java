a747e696c25f2b3c3d7cb0de77767ac264a3c915
Universe <tmdwnchlrh3@gmail.com>
2021-09-30 15:58:44 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,32 @@
 package dept;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import jdbc.Connect;
+
 public class DeptDAO {
+	//empDAO static, Connection
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 }
+
